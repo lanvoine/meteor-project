e233826a971bd9b6752d82be236c600b07fa613a
lanvoine <64321770+lanvoine@users.noreply.github.com>
2022-07-13 18:28:50 +0800

    Added MobilePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DesktopPage from './components/DesktopPage';
+import MobilePage from './components/MobilePage';
 
 function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const breakpoint = 620;
+
+  useEffect(() => {
+    const handleWindowResize = () => setWidth(window.innerWidth)
+    window.addEventListener("resize", handleWindowResize);
+
+    // Return a function from the effect that removes the event listener
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
+
   return (
-    <div className="App">
-      <DesktopPage/>
-    </div>
+    width < breakpoint ? <MobilePage /> : <DesktopPage />
   );
 }
 
